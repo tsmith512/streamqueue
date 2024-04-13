c5bdd9c0e0b82e55eb581ce2aa8ec4e363769dcb
Taylor Smith <tsmith@tsmithcreative.com>
2024-04-13 11:25:17 -0500

    Update with my own preferences

--- src/index.ts
+++ src/index.ts
@@ -12,32 +12,32 @@
  */
 
 export interface Env {
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	MY_QUEUE: Queue;
+  // Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
+  MY_QUEUE: Queue;
 }
 
 export default {
-	// Our fetch handler is invoked on a HTTP request: we can send a message to a queue
-	// during (or after) a request.
-	// https://developers.cloudflare.com/queues/platform/javascript-apis/#producer
-	async fetch(req: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		// To send a message on a queue, we need to create the queue first
-		// https://developers.cloudflare.com/queues/get-started/#3-create-a-queue
-		await env.MY_QUEUE.send({
-			url: req.url,
-			method: req.method,
-			headers: Object.fromEntries(req.headers),
-		});
-		return new Response('Sent message to the queue');
-	},
-	// The queue handler is invoked when a batch of messages is ready to be delivered
-	// https://developers.cloudflare.com/queues/platform/javascript-apis/#messagebatch
-	async queue(batch: MessageBatch<Error>, env: Env): Promise<void> {
-		// A queue consumer can make requests to other endpoints on the Internet,
-		// write to R2 object storage, query a D1 Database, and much more.
-		for (let message of batch.messages) {
-			// Process each message (we'll just log these)
-			console.log(`message ${message.id} processed: ${JSON.stringify(message.body)}`);
-		}
-	},
+  // Our fetch handler is invoked on a HTTP request: we can send a message to a queue
+  // during (or after) a request.
+  // https://developers.cloudflare.com/queues/platform/javascript-apis/#producer
+  async fetch(req: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    // To send a message on a queue, we need to create the queue first
+    // https://developers.cloudflare.com/queues/get-started/#3-create-a-queue
+    await env.MY_QUEUE.send({
+      url: req.url,
+      method: req.method,
+      headers: Object.fromEntries(req.headers),
+    });
+    return new Response('Sent message to the queue');
+  },
+  // The queue handler is invoked when a batch of messages is ready to be delivered
+  // https://developers.cloudflare.com/queues/platform/javascript-apis/#messagebatch
+  async queue(batch: MessageBatch<Error>, env: Env): Promise<void> {
+    // A queue consumer can make requests to other endpoints on the Internet,
+    // write to R2 object storage, query a D1 Database, and much more.
+    for (let message of batch.messages) {
+      // Process each message (we'll just log these)
+      console.log(`message ${message.id} processed: ${JSON.stringify(message.body)}`);
+    }
+  },
 };
